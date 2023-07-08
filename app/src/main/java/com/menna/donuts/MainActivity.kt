77ca20fdb2a6bc55ae45a7eef5c2204f6197a1ef
Mennaakram <mennaakram976@gmail.com>
2023-07-08 23:37:08 +0300

    Initial commit

---
+++ app/src/main/java/com/menna/donuts/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.menna.donuts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.menna.donuts.ui.theme.DonutsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
